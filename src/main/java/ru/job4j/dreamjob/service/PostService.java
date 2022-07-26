85e1655ba7791faf127480e349449e7914e980cc
Windows <ivinjc@gmail.com>
2022-07-26 20:47:11 +0300

    Слоеная архитектура. [#504851]

---
+++ src/main/java/ru/job4j/dreamjob/service/PostService.java
@@ -0,0 +1,35 @@
+package ru.job4j.dreamjob.service;
+
+import ru.job4j.dreamjob.model.Post;
+import ru.job4j.dreamjob.persistence.PostStore;
+
+import java.util.Collection;
+
+public class PostService {
+    private static final PostService INST = new PostService();
+    private final PostStore store;
+
+    private PostService() {
+        store = PostStore.instOf();
+    }
+
+    public static PostService instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return store.findAll();
+    }
+
+    public boolean add(Post post) {
+        return store.add(post);
+    }
+
+    public Post findById(int id) {
+        return store.findById(id);
+    }
+
+    public Post update(Post post) {
+        return store.update(post);
+    }
+}
