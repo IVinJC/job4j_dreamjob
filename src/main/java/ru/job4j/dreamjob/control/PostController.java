8fd4528b5a52ec8fe699d42f9e84a470f7562e14
Windows <ivinjc@gmail.com>
2022-07-27 18:54:07 +0300

    Связь слоев через Spring DI. [#504856]

--- src/main/java/ru/job4j/dreamjob/control/PostController.java
+++ src/main/java/ru/job4j/dreamjob/control/PostController.java
@@ -7,16 +7,21 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dreamjob.model.Post;
-import ru.job4j.dreamjob.persistence.PostStore;
+import ru.job4j.dreamjob.service.PostService;
+
 import java.time.LocalDateTime;
 
 @Controller
 public class PostController {
-    private final PostStore store = PostStore.instOf();
+    PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
 
     @GetMapping("/posts")
     public String posts(Model model) {
-        model.addAttribute("posts", store.findAll());
+        model.addAttribute("posts", postService.findAll());
         return "posts";
     }
 
@@ -41,20 +46,20 @@ public class PostController {
     @PostMapping("/createPost")
     public String createPost(@ModelAttribute Post post) {
         post.setCreated(LocalDateTime.now());
-        store.add(post);
+        postService.add(post);
         return "redirect:/posts";
     }
 
     @PostMapping("/updatePost")
     public String updatePost(@ModelAttribute Post post) {
         post.setCreated(LocalDateTime.now());
-        store.update(post);
+        postService.update(post);
         return "redirect:/posts";
     }
 
     @GetMapping("/formUpdatePost/{postId}")
     public String formUpdatePost(Model model, @PathVariable("postId") int id) {
-        model.addAttribute("post", store.findById(id));
+        model.addAttribute("post", postService.findById(id));
         return "updatePost";
     }
 }
--- src/main/java/ru/job4j/dreamjob/service/PostService.java
+++ src/main/java/ru/job4j/dreamjob/service/PostService.java
@@ -1,10 +1,11 @@
 package ru.job4j.dreamjob.service;
 
+import org.springframework.stereotype.Service;
 import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.persistence.PostStore;
 
 import java.util.Collection;
-
+@Service
 public class PostService {
     private static final PostService INST = new PostService();
     private final PostStore store;
